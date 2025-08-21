b55f2147099ccb3da54dbdee33c3f5607ec36c6e
Kishanthrm <kishanthrm@gmail.com>
2025-08-21 14:05:51 +0530

    Initial commit

---
+++ frontend/job-portal/src/pages/LandingPage/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+// import Header from "./components/Header"
+const LandingPage = () => {
+  return (
+    <div className= 'min-h-screen' >
+            {/* <Header /> */}
+    </div>
+  )
+}
+
+export default LandingPage
